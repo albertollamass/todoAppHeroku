fed43c4457bc8cb0d3dcbec3bc0e131fc9365681
albertollamass <alberto.ll.go@gmail.com>
2022-10-15 12:16:00 +0200

    Preparing for deployment on Heroku

--- learnDjango/settings.py
+++ learnDjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sl-(qg0!x143s5(so4s02jxm%^jt+$s-x9@6q7u84$ivale_5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
